bf4b30eb05874549b2128948415fbb567ddf74dd
HarisFarooq23 <harisnetbackup@gmail.com>
2025-12-07 13:06:11 +0500

    Add files via upload
    student avl code block

---
+++ studentavl.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+struct Student {
+    int studentID;
+    int studentpassword;
+    Student* left;
+    Student* right;
+    
+    Student(int id, int password);
+
+
+
+};
+
+
+
+// ==================== AVL ====================
+// class StudentAVL {
+// private:
+//     Student* root;
+    
+//     Student* insert(Student* node, int id, int password);
+//     Student* search(Student* node, int id);
+    
+// public:
+//     StudentAVL();
+//     void addStudent(int id, int password);
+//     bool verifyStudent(int id);
+//     void displayStudent(int id);
+// };
+
+
+Student* StudentAVL(Student* id ,  ){
+
+}
+
+int main() {
+     
+    Student*  root;
+
+
+    return 0;
+}
